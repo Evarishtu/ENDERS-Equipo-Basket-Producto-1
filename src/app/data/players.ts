2ac56d0dda1ec91f33cfee92ababe474535b1219
Evarishtu <edongua@uoc.edu>
2026-03-14 17:01:14 +0100

    Initial commit

---
+++ src/app/data/players.ts
@@ -0,0 +1,55 @@
+import { Player } from "../models/player";
+
+export const players: Player[] = [
+
+  {
+    id: 1,
+    nombre: "Alicia",
+    apellidos: "Pérez",
+    posicion: "Base",
+    edad: 24,
+    altura: 1.85,
+    foto: "assets/images/jugador1.png"
+  },
+
+  {
+    id: 2,
+    nombre: "Elena",
+    apellidos: "Díaz",
+    posicion: "Escolta",
+    edad: 26,
+    altura: 1.9,
+    foto: "assets/images/jugador2.png"
+  },
+
+  {
+    id: 3,
+    nombre: "Luis",
+    apellidos: "García",
+    posicion: "Alero",
+    edad: 28,
+    altura: 2.0,
+    foto: "assets/images/jugador3.png"
+  },
+
+  {
+    id: 4,
+    nombre: "Carlos",
+    apellidos: "López",
+    posicion: "Ala-Pivot",
+    edad: 23,
+    altura: 2.05,
+    foto: "assets/images/jugador4.png"
+  },
+
+  {
+    id: 5,
+    nombre: "David",
+    apellidos: "Torres",
+    posicion: "Pivot",
+    edad: 30,
+    altura: 2.12,
+    foto: "assets/images/jugador5.png"
+  }
+];
+
